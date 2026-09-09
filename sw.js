34bfac429f64f18a48f45834a4e331d47f6338f7
poposu999 <poposu999@gmail.com>
2026-09-09 21:12:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "money-rule-app-v47";
-const APP_SHELL = ["./", "./index.html", "./style.css?v=46", "./app.js?v=46", "./manifest.json"];
+const CACHE_NAME = "money-rule-app-v48";
+const APP_SHELL = ["./", "./index.html", "./style.css?v=48", "./app.js?v=48", "./manifest.json"];
 
 self.addEventListener("install", event => {
   event.waitUntil(
